3d68dded9e8e80b1348b5c8e867f4c3a40e5a3a6
jyk190121 <46877384+jyk190121@users.noreply.github.com>
2026-03-30 15:27:00 +0900

    [MBA-7] Player 사이즈

---
+++ Assets/2. Scripts/Player.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using Key = UnityEngine.InputSystem.Key;
+
+
+public class Player : MonoBehaviour
+{
+
+    public GameObject playerObject;
+    public float moveSpeed = 5f;
+
+    Animator anim;
+
+    void Start()
+    {
+        playerObject = GetComponent<GameObject>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Keyboard.current == null) return;
+
+        // 1. 현재 입력 축 값을 기반으로 실제 입력 강도(Magnitude)를 계산합니다.
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        // 입력 벡터의 길이를 구합니다. (0 ~ 1 사이의 값)
+        float inputMagnitude = new Vector2(h, v).magnitude;
+
+        // 2. 고정된 moveSpeed 대신, 현재 입력 강도를 애니메이터에 전달합니다.
+        // 이렇게 하면 키를 뗄 때 inputMagnitude가 0이 되어 Idle로 돌아갑니다.
+        anim.SetFloat("Speed", inputMagnitude);
+
+        Move(h, v);
+    }
+
+    // 매개변수를 받도록 Move 함수를 약간 수정하면 더 효율적입니다.
+    void Move(float h, float v)
+    {
+        Vector3 moveDir = new Vector3(h, 0, v);
+
+        if (moveDir.sqrMagnitude > 1f)
+        {
+            moveDir.Normalize();
+        }
+
+        transform.Translate(moveDir * moveSpeed * Time.deltaTime, Space.World);
+    }
+}
